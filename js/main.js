056cb0943ddab1715f436958e8cb4360c026155b
Larry <lzhang124@gmail.com>
2015-11-08 18:07:49 -0500

    basic intro

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
-    $('.airplane').delay('8500').fadeOut('2000');
-    $('.splash').delay('8000').fadeIn('1000');
+    $('.floor').delay('8500').fadeOut('2000');
+    $('.bodyShadow').delay('8500').fadeOut('2000');
+    
+    $('.logo').delay('9000').fadeIn('300');
+    $('#year').delay('10000').fadeIn('300');
+    // $('.splash').delay('11000').fadeIn('300');
 });
