da2e6ec57a5e667e9605b2e69126f6ab2781aaa4
Larry <lzhang124@gmail.com>
2015-11-08 21:57:42 -0500

    conditional clicking

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     var cookie = getCookie('animate');
+    var click = false;
     if (cookie !== 'false') {
         document.cookie="animate=false";
         $('.floor').delay('8500').fadeOut('2000');
@@ -8,6 +9,7 @@ $(document).ready(function(){
         $('.splash').delay('10000').fadeIn('1000');
         
         setTimeout(function() {
+            click = true;
             $('.camera').css({
                 '-webkit-animation': 'none',
                 '-moz-animation': 'none',
@@ -16,8 +18,12 @@ $(document).ready(function(){
                 '-moz-transform': 'translate3d(0px, 0px, -80px)',
                 'transform': 'translate3d(0px, 0px, -80px)'
             });
+            $('.airplane').css({
+                'cursor': 'pointer'
+            });
         }, 10000);
     } else {
+        click = true;
         $('.camera').css({
             '-webkit-animation': 'none',
             '-moz-animation': 'none',
@@ -25,6 +31,9 @@ $(document).ready(function(){
             '-webkit-transform': 'translate3d(0px, 0px, -80px)',
             '-moz-transform': 'translate3d(0px, 0px, -80px)',
             'transform': 'translate3d(0px, 0px, -80px)'
+        });
+        $('.airplane').css({
+            'cursor': 'pointer'
         });
         $('.floor').hide();
         $('.bodyShadow').hide();
@@ -34,33 +43,46 @@ $(document).ready(function(){
 
     var cameraClass = 'angle0';
     $('.airplane').click(function() {
-        $('.splash').fadeOut('500');
-        $('.logo').fadeOut('500');
-        $('.bodyShadow').delay('500').fadeIn('500');
-        $('.floor').delay('500').fadeIn('500');
-
-        if (Math.random() >= .5) {
-            newClass = 'angle1';
-        } else {
-            newClass = 'angle2';
-        }
-        
-        $('.camera').removeClass(cameraClass);
-        setTimeout(function() {
-            $('.camera').css({
-                '-webkit-animation': '',
-                '-moz-animation': '',
-                'animation': '',
+        if (click) {
+            click = false;
+            $('.airplane').css({
+                'cursor': ''
             });
-            $('.camera').addClass(newClass);
-            cameraClass = newClass;
-        }, 1500);
+            $('.splash').fadeOut('500');
+            $('.logo').fadeOut('500');
+            $('.bodyShadow').delay('500').fadeIn('500');
+            $('.floor').delay('500').fadeIn('500');
 
-        $('.floor').delay('9000').fadeOut('1000');
-        $('.bodyShadow').delay('9000').fadeOut('1000');
-        $('.logo').delay('10000').fadeIn('1000');
-        $('.splash').delay('10000').fadeIn('1000');
-    })
+            if (Math.random() >= .5) {
+                newClass = 'angle1';
+            } else {
+                newClass = 'angle2';
+            }
+            
+            $('.camera').removeClass(cameraClass);
+            setTimeout(function() {
+                $('.camera').css({
+                    '-webkit-animation': '',
+                    '-moz-animation': '',
+                    'animation': '',
+                });
+                $('.camera').addClass(newClass);
+                cameraClass = newClass;
+            }, 1500);
+
+            $('.floor').delay('9000').fadeOut('1000');
+            $('.bodyShadow').delay('9000').fadeOut('1000');
+            $('.logo').delay('10000').fadeIn('1000');
+            $('.splash').delay('10000').fadeIn('1000');
+
+            setTimeout(function() {
+                click = true;
+                $('.airplane').css({
+                    'cursor': 'pointer'
+                });
+            }, 10000);
+        }
+    });
 });
 
 function getCookie(cname) {
