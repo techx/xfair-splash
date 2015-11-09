8ff86a5d1f40ec02757b90c27cf28e9ff6e14432
Larry <lzhang124@gmail.com>
2015-11-08 20:50:00 -0500

    animations

--- js/main.js
+++ js/main.js
@@ -2,20 +2,49 @@ $(document).ready(function(){
     var cookie = getCookie('animate');
     if (cookie !== 'false') {
         document.cookie="animate=false"
-        animate();
+        $('.floor').delay('8500').fadeOut('2000');
+        $('.bodyShadow').delay('8500').fadeOut('2000');
+        $('.logo').delay('9000').fadeIn('1000');
+        $('.splash').delay('10000').fadeIn('1000');
     } else {
-        animate()
-        // $('.camera').css({
-        //     'animation': 'none',
-        //     '-webkit-transform': 'translate3d(0px, 0px, -80px)',
-        //     '-moz-transform': 'translate3d(0px, 0px, -80px)',
-        //     'transform': 'translate3d(0px, 0px, -80px)'
-        // });
-        // $('.floor').hide();
-        // $('.bodyShadow').hide();
-        // $('.logo').show();
-        // $('.splash').show();
+        $('.camera').css({
+            '-webkit-animation': 'none',
+            '-moz-animation': 'none',
+            'animation': 'none',
+            '-webkit-transform': 'translate3d(0px, 0px, -80px)',
+            '-moz-transform': 'translate3d(0px, 0px, -80px)',
+            'transform': 'translate3d(0px, 0px, -80px)'
+        });
+        $('.floor').hide();
+        $('.bodyShadow').hide();
+        $('.logo').show();
+        $('.splash').show();
     }
+
+    $('.airplane').click(function() {
+        $('.splash').fadeOut('1000');
+        $('.logo').fadeOut('1000');
+        $('.bodyShadow').fadeIn('1000');
+        $('.floor').fadeIn('1000');
+
+        setTimeout(function() {
+            $('#camera').removeClass('camera')
+        });
+        $('.camera').delay('1000').css({
+            '-webkit-animation': '',
+            '-moz-animation': '',
+            'animation': '',
+            '-webkit-transform': '',
+            '-moz-transform': '',
+            'transform': ''
+        });
+        $('#camera').delay('1000').addClass('newcamera');
+
+        $('.floor').delay('8500').fadeOut('2000');
+        $('.bodyShadow').delay('8500').fadeOut('2000');
+        $('.logo').delay('9000').fadeIn('1000');
+        $('.splash').delay('10000').fadeIn('1000');
+    })
 });
 
 function getCookie(cname) {
@@ -27,17 +56,4 @@ function getCookie(cname) {
         if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
     }
     return "";
-}
-
-function animate() {
-    $('.floor').show();
-    $('.bodyShadow').show();
-    $('.logo').hide();
-    $('.splash').hide();
-
-    $('.floor').delay('8500').fadeOut('2000');
-    $('.bodyShadow').delay('8500').fadeOut('2000');
-    $('.logo').delay('9000').fadeIn('1000');
-    $('.splash').delay('10000').fadeIn('1000');
-    // $('.airplane').delay('9000').css({'z-index': '1'});
 }
